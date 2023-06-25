057355af433ab75d8c5dd57f3d795850dd879c4d
phili <philipsolo4@gmail.com>
2023-06-25 13:35:52 +0100

    Modified with IP address

--- wol_api/web.py
+++ wol_api/web.py
@@ -16,16 +16,30 @@ from flask import Flask, request
 from gevent.pywsgi import WSGIServer
 from wakeonlan import send_magic_packet
 
+devices = {
+    'philip': {'mac': '58:11:22:C9:06:69', 'ip_address': '192.168.0.75'}
+}
+
+
+def wake_device(device_name):
+    if device_name in devices:
+        mac, ip = devices[device_name].values()
+        send_magic_packet(mac, ip_address=ip)
+        print('Magic Packet Sent')
+        return True
+    else:
+        print('Device Not Found')
+        return False
+
 
 def app_container(port):
     app = Flask(__name__)
 
-    @app.route("/wol/<mac>", methods=['GET'])
-    def myjd_stop(mac):
+    @app.route("/wol/<device>", methods=['GET'])
+    def myjd_stop(device):
         if request.method == 'GET':
             try:
-                send_magic_packet(mac)
-                print("Waking up: " + mac)
+                wake_device(device)
                 return "Success", 200
             except:
                 return "Failed", 400
